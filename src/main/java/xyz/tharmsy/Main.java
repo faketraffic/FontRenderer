c87970cdd0ba01f11aab4af69195dd0b9711d1dd
omg123 <133595791+faketraffic@users.noreply.github.com>
2024-08-25 18:37:51 +0000

    Update Main.java

--- src/main/java/xyz/tharmsy/Main.java
+++ src/main/java/xyz/tharmsy/Main.java
@@ -2,6 +2,6 @@ package xyz.tharmsy;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!1");
     }
 }
