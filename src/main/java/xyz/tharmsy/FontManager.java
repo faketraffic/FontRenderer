0cc55bc2bf763d393188277b8e0b1d96e77da6b9
tharm <tharmsy@gmail.com>
2024-08-25 18:45:22 +0000

    FontMananger TextRenderer started

--- src/main/java/xyz/tharmsy/FontManager.java
+++ src/main/java/xyz/tharmsy/FontManager.java
@@ -1,4 +1,31 @@
 package xyz.tharmsy;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class FontManager {
+    private Map<String, Font> fonts;
+
+    public FontManager() {
+        fonts = new HashMap<>();
+    }
+
+    public void loadFont(String fontName, String fontFilePath) {
+        try {
+            Font font = Font.createFont(Font.TRUETYPE_FONT, new File(fontFilePath));
+            fonts.put(fontName, font);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(font);
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Font getFont(String fontName, int style, float size) {
+        Font font = fonts.getOrDefault(fontName, new Font("Serif", style, (int)size));
+        return font.deriveFont(style, size);
+    }
 }
